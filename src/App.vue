3956675132d2c0e3055d9fb01856b5484de4396d
Joachim Stroh <joachim@sceenius.com>
2019-02-19 20:21:59 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Meetings />
+</template>
+
+<script>
+import Meetings from "./components/Meetings.vue";
+
+export default {
+  name: "App",
+  components: {
+    Meetings
+  }
+};
+</script>
+
+<style>
+#app {
+  @import "https://fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons";
+
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
