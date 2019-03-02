cd2f495706491ef0f961172d6dd3feb327bf815c
Joachim Stroh <joachim@sceenius.com>
2019-03-01 19:00:06 -0500

    Router fix needs '/'

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,11 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
+      path: "/",
+      name: "Meetings",
+      component: Meetings
+    },
+    {
       path: "/:domain",
       name: "Meetings",
       component: Meetings,
